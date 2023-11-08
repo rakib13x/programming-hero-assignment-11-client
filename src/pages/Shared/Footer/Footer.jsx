ea63f208f4d1ea4b86a03c1daed016ba82d6693b
rakib13x <rakib13x@gmail.com>
2023-11-08 23:09:43 +0600

    footer section modified

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -2,12 +2,40 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
-      <Link to="/" className="font-bold text-xl">
-        MASSIMO
-      </Link>
-      <p>© ALL RIGHTS RESERVED.</p>
-    </div>
+    <>
+      <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
+        <Link to="/" className="font-bold text-xl">
+          MASSIMO
+        </Link>
+        <p>© ALL RIGHTS RESERVED.</p>
+      </div>
+      <div>
+        <div>
+          <footer class="footer p-10 bg-red-50 text-red-500">
+            <nav>
+              <header class="footer-title">Services</header>
+              <a class="link link-hover">Items</a>
+              <a class="link link-hover">Table</a>
+              <a class="link link-hover">Orders</a>
+              <a class="link link-hover">Advertisement</a>
+            </nav>
+            <nav>
+              <header class="footer-title">Company</header>
+              <a class="link link-hover">About us</a>
+              <a class="link link-hover">Contact</a>
+              <a class="link link-hover">Jobs</a>
+              <a class="link link-hover">Press kit</a>
+            </nav>
+            <nav>
+              <header class="footer-title">Legal</header>
+              <a class="link link-hover">Terms of use</a>
+              <a class="link link-hover">Privacy policy</a>
+              <a class="link link-hover">Cookie policy</a>
+            </nav>
+          </footer>
+        </div>
+      </div>
+    </>
   );
 };
 export default Footer;
